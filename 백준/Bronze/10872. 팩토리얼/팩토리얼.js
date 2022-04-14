c24237751ed2f1e522e0c27047857c8546e6fb29
Minchan Lee <shigatsu970704@gmail.com>
2022-04-14 16:26:35 +0900

    [Bronze III] Title: 팩토리얼, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const input = parseInt(fs.readFileSync("/dev/stdin").toString().trim());
+let result = 1;
+
+for (let i = 1; i <= input; i++) {
+  result *= i;
+}
+
+console.log(result);
