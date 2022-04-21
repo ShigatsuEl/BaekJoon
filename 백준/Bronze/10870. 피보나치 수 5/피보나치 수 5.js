cba2f8b5e8069a42289aba6f136575b3ee0280ba
Minchan Lee <shigatsu970704@gmail.com>
2022-04-21 13:37:20 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 172 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,10 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "example.txt";
+const input = parseInt(require("fs").readFileSync(filePath).toString().trim());
+
+function fibonacci(n) {
+  if (n === 0) return 0;
+  else if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(input));
