8130a3ae819d9a2e74da5a84c863b0aebf9e7980
Minchan Lee <shigatsu970704@gmail.com>
2022-04-22 17:31:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 140 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.js
@@ -0,0 +1,4 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "example.txt";
+const input = require("fs").readFileSync(filePath).toString().trim();
+
+console.log(input.charCodeAt(0));
