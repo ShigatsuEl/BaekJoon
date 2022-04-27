40a1781f689b0d9b73a23a7a2a61a91963663c47
Minchan Lee <shigatsu970704@gmail.com>
2022-04-27 17:48:07 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.js
@@ -0,0 +1,10 @@
+let filePath = process.platform === "linux" ? "dev/stdin" : "example.txt";
+let input = require("fs").readFileSync(filePath).toString().split(" ");
+const x = Number(input[0]);
+const y = Number(input[1]);
+const w = Number(input[2]);
+const h = Number(input[3]);
+
+const array = [x, w - x, y, h - y];
+const answer = Math.min(...array);
+console.log(answer);
