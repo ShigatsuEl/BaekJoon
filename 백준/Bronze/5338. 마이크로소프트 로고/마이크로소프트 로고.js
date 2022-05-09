b745dd25546c92a61ee8f3ceb6d4e56ad4a44378
Minchan Lee <shigatsu970704@gmail.com>
2022-05-09 18:00:57 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 112 ms, Memory: 9276 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.js
@@ -0,0 +1,9 @@
+let answer = "";
+
+answer += "       _.-;;-._" + "\n";
+answer += "'-..-'|   ||   |" + "\n";
+answer += "'-..-'|_.-;;-._|" + "\n";
+answer += "'-..-'|   ||   |" + "\n";
+answer += "'-..-'|_.-''-._|";
+
+console.log(answer);
